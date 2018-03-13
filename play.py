557e5a8da0e78cd4447c7d7232d22748aa9bde9d
Tien <34505433+nhtLeNa@users.noreply.github.com>
2018-03-13 23:33:21 +0700

    Add files via upload

--- play.py
+++ play.py
@@ -1,36 +1,73 @@
 import pygame
 pygame.init()
-
 win_sound = pygame.mixer.Sound("win.ogg")
 lose_sound = pygame.mixer.Sound("lose.ogg")
 pygame.mixer.music.load("play_turtle.ogg")
 
+def chart(win_lose,total_score):
+    import matplotlib.pyplot as plt  # chart
+    plt.rcdefaults()  # horizontal
+    import numpy as np
+
+    print(total_score)
+    print(win_lose[0],win_lose[1])
+    plt.subplot(1,2, 1)
+    #Data
+    betwin=[win_lose[0],win_lose[1]]
+    lab=['win','lose']
+    labels=lab
+    size=betwin
+    color=['Yellow','Green']
+    explode=(0,0)
+
+    #plot
+    plt.pie(betwin,explode=explode,labels=labels,autopct='%1.1f%%',colors=color,shadow=False,startangle=90)
+    plt.title('The result:')
+    plt.axis('equal')
+
+    #horizontal
+    plt.subplot(1, 2, 2)
+    score = [0,0,0,0]
+    for i in range(4):
+        score[i] = total_score[i]
+    character = ['red','yellow', 'green', 'blue']
+    objects = character
+    y_pos = np.arange(len(objects))
+    performance = score
+    plt.barh(y_pos, performance, align='center', alpha=0.5)
+    plt.yticks(y_pos, objects)
+    plt.xlabel('TOTAL SCORE ')
+    plt.title('TURTLE NAME ')
+
+    plt.tight_layout()
+    plt.show()
+
 from start import *
 from random import randint
-
-def movingbackward(char, number):#di lui
-    char.setheading(180)
-    char.forward(0 - number)
 
 def play_music():
     pygame.mixer.music.play(-1)
 
-def racing():
+def movingbackward(char, number):  # di lui
+    char.setheading(180)
+    char.forward(0 - number)
 
+
+def racing(pointlist):
     play_music()
-    d1 = 0 #random vi tri di truoc
+    d1 = 0  # random vi tri di truoc
     d2 = 0
     d3 = 0
     d4 = 0
-    rank_val = 0 #bien xep hang
+    rank_val = 0  # bien xep hang
     while d1 < length or d2 < length or d3 < length or d4 < length:
         if d1 < length:
-            random = randint(1, 4) #cho random tu 1->4
+            random = randint(1, 4)  # cho random tu 1->4
         else:
-            random = 0 #neu toi dich roi thi k dua nua
+            random = 0  # neu toi dich roi thi k dua nua
         if d2 < length:
             random2 = randint(1, 4)
-            while random2 == random: #random nhung phai khac voi so da duoc random truoc
+            while random2 == random:  # random nhung phai khac voi so da duoc random truoc
                 random2 = randint(1, 4)
         else:
             random2 = 0
@@ -49,11 +86,11 @@ def racing():
         List = [random, random2, random3, random4]
         for count in range(4):
             if List[count] == 1 and d1 < length:
-                move = randint(b_turtle, f_turtle) #random buoc di
+                move = randint(b_turtle, f_turtle)  # random buoc di
                 if move == 0:
-                    move = randint(b_turtle, f_turtle) #khong cho dung lai
+                    move = randint(b_turtle, f_turtle)  # khong cho dung lai
                 if move < 0:
-                    if blue.xcor() <= -140: #o vach xuat phat khong duoc di lui
+                    if blue.xcor() <= -140:  # o vach xuat phat khong duoc di lui
                         move = randint(0, f_turtle)
                     else:
                         movingbackward(blue, move)
@@ -116,32 +153,59 @@ def racing():
                 rank_val += 1
                 pointlist[3] += rank_val
 
-
-def winner(): #kiem tra thang thua
-    m = 5
-    for c in range(4):
-        if pointlist[c] < m:
-            m = pointlist[c]
-    win = namelist[c]
+def winner(pointlist,win_lose,total_score):  # kiem tra thang thua
+    win = 1
+    for i in range(4):
+        if pointlist[i] == 1:
+            win = i
+            total_score[i] += 1
+    print(pointlist)
+    print(total_score)
     print('The winner is:\n', win)
     if bet == win:
+        pygame.mixer.music.stop()
         pygame.mixer.Sound.play(win_sound)
-        pygame.mixer.music.stop()
-        print('You won the bet')
+        win_lose[0] += 1
+        #print('You won the bet')
     else:
-        pygame.mixer.Sound.play(lose_sound)
         pygame.mixer.music.stop()
-        print('You lost the bet')
+        pygame.mixer.Sound.play(lose_sound)
+        win_lose[1] += 1
+        #print('You lost')
+    print(win_lose[0],win_lose[1])
 
-#while True:
-    pointlist = [0, 0, 0, 0]
-    racing()
-    print(pointlist)
-    winner()
-   # tmp = input('You want to play again ? (Y/N) ')
-   # if tmp == 'N':
-   #     break
+def update_point(point, pointlist):
+    for count in range(4):
+        pos = pointlist[count]
+        if   pos == 1:
+             point[count].first += 1
+        elif pos == 2:
+            point[count].second += 1
+        elif pos == 3:
+            point[count].third += 1
+        else:
+            point[count].fourth += 1
+
+def create_point_struct(_class, num):
+    tmp = list()
+    for count in range(num):
+        tmp.append(_class)
+    return tmp
+
+def play_continue_game(pointlist,win_lose,total_score):
+    racing(pointlist)
+    #print(pointlist)
+    #point = create_point_struct(point_history, 4)
+    #update_point(point, pointlist)
+    winner(pointlist,win_lose,total_score)
+    chart(win_lose,total_score)
 
 
+def play_new_game(pointlist,win_lose, total_score):
 
+    play_continue_game(pointlist,win_lose,total_score)
 
+pointlist = [0, 0, 0, 0]
+total_score = [0,0,0,0]
+win_lose = [0,0]
+play_new_game(pointlist,win_lose,total_score)
--- start.py
+++ start.py
@@ -1,61 +1,85 @@
+
 from turtle import *
 import time
 
 f_turtle = 10
 b_turtle = -3
+
+class point_history:
+    fisrt = 0
+    second = 0
+    third = 0
+    fourth = 0
+
 #man hinh
+
 screen=Screen()
-may=screen.textinput('yes',50)
+may=screen.textinput("Name","Enter your character name:");
 ex=Turtle()
 ex.hideturtle()
-for count in range(4): #dem thoi gian
+
+
+for count in range(1,4,1): #dem thoi gian
     ex.write(count,move=False,align='left',font=("Arial",16,"normal"))
     time.sleep(1)
     ex.clear()
-print(may)
 
 
-def create_character(char): #ham con thay the cho 3 dong trong def character set
+
+def create_character(char, loca_y): #ham con thay the cho 3 dong trong def character set
     char.showturtle()
     char.penup()
-    char.goto(-160, 80)
+    char.goto(-160, loca_y)
     char.pendown()
+    char.penup()
 
 
 def character_set(char):
     if char == 'T': #set con rua
         red.color('red')
         red.shape('turtle')
-        create_character(red)
-        #red.showturtle()
-        #red.penup()
-        #red.goto(-160, 80)
-        #red.pendown()
-        namelist[1] = input('Name:\n')
+        create_character(red, 80)
+        namelist[0] = screen.textinput("Name","Enter your character name:");
 
         yellow.color('yellow')
         yellow.shape('turtle')
-        yellow.showturtle()
-        yellow.penup()
-        yellow.goto(-160, 40)
-        yellow.pendown()
-        namelist[3] = input('Name:\n')
+        create_character(yellow, 40)
+        namelist[1] = screen.textinput("Name","Enter your character name:");
 
         green.color('green')
         green.shape('turtle')
-        green.showturtle()
-        green.penup()
-        green.goto(-160, 0)
-        green.pendown()
-        namelist[2] = input('Name:\n')
+        create_character(green, 0)
+        namelist[2] = screen.textinput("Name","Enter your character name:");
 
         blue.color('blue')
         blue.shape('turtle')
-        blue.showturtle()
-        blue.penup()
-        blue.goto(-160, -40)
-        blue.pendown()
-        namelist[0] = input('Name:\n')
+        create_character(blue, -40)
+        namelist[3] = screen.textinput("Name","Enter your character name:");
+
+    elif char == 'D':
+        red_disa = "red.gif"
+        screen.addshape("red.gif")
+        red.shape(red_disa)
+        create_character(red, 80)
+        namelist[0] = screen.textinput("Name","Enter your character name:");
+
+        yellow_disa = "yellow.gif"
+        screen.addshape("yellow.gif")
+        yellow.shape(yellow_disa)
+        create_character(yellow, 40)
+        namelist[1] = screen.textinput("Name","Enter your character name:");
+
+        green_disa = "green.gif"
+        screen.addshape("green.gif")
+        green.shape(green_disa)
+        create_character(green, 0)
+        namelist[2] = screen.textinput("Name","Enter your character name:");
+
+        blue_disa = "blue.gif"
+        screen.addshape("blue.gif")
+        blue.shape(blue_disa)
+        create_character(blue, -40)
+        namelist[3] = screen.textinput("Name","Enter your character name:");
 
 #ve duong dua
 def therace(col):
@@ -75,16 +99,20 @@ def therace(col):
         forward(20)
 
 
-speed(10) #toc do
+speed(10)  # toc do
 penup()
 goto(-140, 140)
 penup()
-column = input('Enter number of columns( Column 0 included):\n')
-therace(int(column))
-character = input('Choose your character ( R-Rabbits, T-Turtle.....(update later):\n')
 
-namelist = ['', '', '', ''] #ten cua nhan vat
-#khoi tao turtle cho tung con
+while True:
+    column = screen.textinput("", "Enter number of column:")
+    if int(column) <= 40:
+        break
+therace(int(column))
+character = screen.textinput("", "Choose your character (R\T):\n")
+
+namelist = ['', '', '', '']  # ten cua nhan vat
+    # khoi tao turtle cho tung con
 red = Turtle()
 red.hideturtle()
 blue = Turtle()
@@ -96,5 +124,6 @@ yellow.hideturtle()
 character_set(character)
 
 print(namelist)
-bet = input('Which one would be the winner?: ')#ca cuoc
-length=int(column)*20 # do dai duong dua
+bet = screen.textinput("", "Which one would be the winner?:")  # ca cuoc
+length = int(column) * 20  # do dai duong dua
+
